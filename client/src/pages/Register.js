a430888f92de56f6d2c84a316c72db0f9073e0f6
Kateattardo <youremail@domain.com>
2023-12-14 15:46:22 -0500

    user registration form

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -6,21 +6,52 @@ export default function Register() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      console.log(name, email, password);
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <div>
       <Jumbotron title="Register" />
 
-      <div className="container">
+      <div className="container mt-5">
         <div className="row">
           <div className="col-md-6 offset-md-3">
-            <input
-              type="text"
-              className="form-control mb-4 p-2"
-              placeholder="Enter your name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              autoFocus
-            />
+            <form onSubmit={handleSubmit}>
+              <input
+                type="text"
+                className="form-control mb-4 p-2"
+                placeholder="Enter your name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                autoFocus
+              />
+
+              <input
+                type="email"
+                className="form-control mb-4 p-2"
+                placeholder="Enter your email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+
+              <input
+                type="password"
+                className="form-control mb-4 p-2"
+                placeholder="Enter your password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+
+              <button className="btn btn-primary" type="submit">
+                Submit
+              </button>
+            </form>
           </div>
         </div>
       </div>
