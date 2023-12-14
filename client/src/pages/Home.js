5275b6818136e9cc198297f8e36e42f35140561f
Kateattardo <youremail@domain.com>
2023-12-14 10:53:08 -0500

    jumbotron

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,9 @@
+import Jumbotron from "../components/cards/Jumbotron";
+
 export default function Home() {
   return (
     <div>
+      <Jumbotron />
       <h1>This is Home Page</h1>
     </div>
   );
