8c4e6d73cc3dfbc4f00920524a7106536a1a6aa7
Kateattardo <youremail@domain.com>
2023-12-14 10:29:51 -0500

    navbar active

--- client/src/components/nav/Menu.js
+++ client/src/components/nav/Menu.js
@@ -1,26 +1,23 @@
+import { NavLink } from "react-router-dom";
+
 export default function Menu() {
   return (
     <>
-      <ul className="nav">
+      <ul className="nav d-flex justify-content-between shadow-sm mb-2">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">
-            Active
-          </a>
+          <NavLink className="nav-link" aria-current="page" to="/">
+            HOME
+          </NavLink>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">
-            Link
-          </a>
+          <NavLink className="nav-link" to="/lgoin">
+            LOGIN
+          </NavLink>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">
-            Link
-          </a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link disabled" aria-disabled="true">
-            Disabled
-          </a>
+          <NavLink className="nav-link" to="/register">
+            REGISTER
+          </NavLink>
         </li>
       </ul>
     </>
